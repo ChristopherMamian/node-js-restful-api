1820f8ed44df8f50157d3a53764ab5d445b8d605
Christopher Mamian <christophermamian@gmail.com>
2015-03-13 23:33:00 -0700

    client model

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
 
+var Client = require('./app/models/client');
+
 router.get('/', function(req, res) {
 	res.json({ message: 'Welcome to my api!'});
 });
